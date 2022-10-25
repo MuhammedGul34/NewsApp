5d650532bca6d752c1dfa18fd0defbcf1e2ce3e3
muhammed gül <muhammedgul.mail@gmail.com>
2022-10-25 16:23:04 +0300

    Initial Commit

---
+++ HaberProjesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaberProjesi
+//
+//  Created by Muhammed Gül on 25.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
